fc38517bd16a8cbd7a2e7894a5700e6957a287fc
AnkurDev <ankurtomar8@outlook.com>
2025-08-18 23:42:07 +0530

    [Junit] Added Compare Peformance to understand unit
    [Desc] Added Compare Peformance test to understand unit
    [Sprint] Daily
    [Scope] New

--- src/test/java/com/JunitAnkur/ArraysCompareTest.java
+++ src/test/java/com/JunitAnkur/ArraysCompareTest.java
@@ -38,4 +38,18 @@ public class ArraysCompareTest {
        // assertArrayEquals(expected,numbers);
 
     }
+
+    @org.junit.Test(timeout = 100)
+    public void testSort_Performance(){
+        int array[] = {12,23,4};
+
+        for(int i= 0; i <=1000000;i++){
+
+            array[0] = i;
+            Arrays.sort(array);
+
+        }
+    }
+
+
 }
